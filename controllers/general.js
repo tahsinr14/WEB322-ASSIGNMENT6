15739c2b1e67ad93223abd8cdccb78cdc445e843
Tahsin Rahman <trahman31@myseneca.ca>
2021-04-20 15:57:48 -0400

    Updated general controller

--- controllers/general.js
+++ controllers/general.js
@@ -1,28 +1,43 @@
 
 const express = require('express')
 const router = express.Router();
-const  { getTopMeal, getAllMeal}  
-= require("../models/data.js"); 
+const  { getTopMeal, getAllMeal, veganMeals}  
+= require("../models/data.js");
+const meals = require('../models/mealKit');
 
 
 //home
 // setup a 'route' to listen on the default url path (http://localhost)
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
     res.render("general/home", {
-        meals: getTopMeal(),
+        meals: await getTopMeal(),
         title: "Euphoria Meals"
     });
 });
 
 //on the menu
 // setup another route to listen on /OnTheMenu
-router.get("/onTheMenu", (req, res) => {
+router.get("/onTheMenu", async (req, res) => {
     res.render("general/onTheMenu", {
-        mealKit: getAllMeal(),
+        meals: await getAllMeal(),
         title: "On The Menu"
     });
 });
 
+// meal kit description
+router.get("/:meal/meal", async (req, res) => {
+    const meal = await meals.findById(req.params.meal);
+	const mealInCart = req.session.cart.find(item => item.id == meal._id);
+	res.render('general/meal', {mealInCart, meal: meal.toJSON()});
+});
+
+// vegan menu
+router.get("/veganMenu", async (req, res) => {
+    res.render("general/vegan", {
+        meals: await veganMeals(),
+        title: "Vegan Menu"
+    });
+});
 
 module.exports = router;
 
